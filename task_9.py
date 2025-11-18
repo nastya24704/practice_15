bd523ffb6c183866ec8ace93294e656604257cfd
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:52:22 +0700

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,1 +1,18 @@
+def combin(n: int, k: int) -> int:
+    """
+    Recursive calculation of binomial coefficient C(n, k).
+    """
+    if k == 0 or k == n:
+        return 1
+    else:
+        return combin(n - 1, k - 1) + combin(n - 1, k)
+
+def main():
+    n = int(input())
+    k = int(input())
+    result = combin(n, k)
+    print(result)
+
+if __name__ == "__main__":
+    main()
 
