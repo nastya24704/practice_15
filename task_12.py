39e954fd54e3c41a2532ce35465f9f1ecc38beb2
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:54:12 +0700

    Update task_12.py

--- task_12.py
+++ task_12.py
@@ -1,1 +1,21 @@
+def search(a: list, x: int) -> int:
+    '''
+    Search for element x in list recursively.
+    Return 1 if found, else 0.
+    '''
+    if not a:
+        return 0
+    elif a[0] == x:
+        return 1
+    else:
+        return search(a[1:], x)
+
+def main():
+    a = []
+    x = int(input())
+    result = search(a, x)
+    print(bool(result))
+
+if __name__ == "__main__":
+    main()
 
