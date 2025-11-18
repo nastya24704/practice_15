f8640da942d16fefa743e9c5e1f0093e81f2ab77
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:54:56 +0700

    Update task_13.py

--- task_13.py
+++ task_13.py
@@ -1,1 +1,21 @@
+def odd_list(a: list, n: int) -> list:
+    '''
+    Return list of even numbers from first n elements of a.
+    '''
+    if n == 0:
+        return []
+    else:
+        result = odd_list(a, n - 1)
+        if a[n - 1] % 2 == 0:
+            result.append(a[n - 1])
+        return result
+
+def main():
+    a = []
+    n = len(a)
+    result = odd_list(a, n)
+    print(result)
+
+if __name__ == "__main__":
+    main()
 
