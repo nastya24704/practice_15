4bc91a1a5c9271805157902e332c310e29fe62c8
nastya24704 <nastazeravina@gmail.com>
2025-11-18 11:02:17 +0700

    Update task_16.py

--- task_16.py
+++ task_16.py
@@ -1,1 +1,20 @@
+def ten_to_n(x: int, n: int) -> str:
+    '''
+    Recursively converts a natural number x from decimal to base n (2 <= n <= 16) without using nested functions.
+    '''
+    digits = "0123456789ABCDEF"  
+    if x < n:
+        return digits[x]
+    else:
+        return ten_to_n(x // n, n) + digits[x % n]
 
+
+def main():
+    x = int(input())
+    n = int(input())
+    result = ten_to_n(x, n)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
