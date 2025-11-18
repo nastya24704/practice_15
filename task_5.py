c440d6267b47d1ca89a7193e2cf1e778b5989831
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:50:47 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,1 +1,20 @@
+def mod_number(a: int, b: int) -> int:
+    '''
+    Compute a mod b using recursion.
+    :param a: Dividend
+    :param b: Divisor
+    :return: Remainder of division
+    '''
+    if a < b:
+        return a
+    else:
+        return mod_number(a - b, b)
 
+def main():
+    a = 17
+    b = 5
+    result = mod_number(a, b)
+    print(result)
+
+if __name__ == "__main__":
+    main()
