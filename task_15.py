db13e4d19b545c448ad287e1ce6bec5590399d4a
nastya24704 <nastazeravina@gmail.com>
2025-11-18 11:00:28 +0700

    Update task_15.py

--- task_15.py
+++ task_15.py
@@ -1,1 +1,16 @@
+def ten_to_bin(x: int) -> str:
+    '''
+    Recursively converts the decimal number x to its binary representation as a string without using an auxiliary function.
+    '''
+    if x == 0:
+        return '0'
+    if x == 1:
+        return '1'
+    return ten_to_bin(x // 2) + str(x % 2)
 
+def main():
+    x = int(input())
+    print(ten_to_bin(x))
+
+if __name__ == "__main__":
+    main()
