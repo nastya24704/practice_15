d6acac4d1e5073f827035f53969dbbceb812e439
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:52:44 +0700

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,1 +1,18 @@
+def maxlist(a: List[int]) -> int:
+    '''
+    Recursively find the maximum element in a list.
+    '''
+    if len(a) == 1:
+        return a[0]
+    else:
+        max_rest = maxlist(a[1:])
+        return a[0] if a[0] > max_rest else max_rest
+
+def main():
+    a = []
+    result = maxlist(a)
+    print(result)
+
+if __name__ == "__main__":
+    main()
 
