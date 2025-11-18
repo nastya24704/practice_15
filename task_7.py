9b837efcf2940d7e019cc90d5c17a785f464dd56
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:51:33 +0700

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,1 +1,18 @@
+def nod(a: int, b: int) -> int:
+    '''
+    Compute GCD of a and b recursively.
+    '''
+    if b == 0:
+        return a
+    else:
+        return nod(b, a % b)
+
+def main():
+    a = int(input())
+    b = int(input())
+    result = nod(a, b)
+    print(result)
+
+if __name__ == "__main__":
+    main()
 
