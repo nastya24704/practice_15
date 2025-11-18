673f11b0db3394b4ca2cc95ee8776622ea67d53b
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:55:17 +0700

    Update task_14.py

--- task_14.py
+++ task_14.py
@@ -1,1 +1,16 @@
+def numbers(x: int) -> None:
+    '''
+    Print digits of x in reverse order.
+    '''
+    print(x % 10)
+    if x >= 10:
+        numbers(x // 10)
+
+def main():
+    x = int(input())
+    print("Digits in reverse order:")
+    numbers(x)
+
+if __name__ == "__main__":
+    main()
 
