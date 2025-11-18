970144e2931f7ee6bc063cccf5c4fa86223a3660
nastya24704 <nastazeravina@gmail.com>
2025-11-18 10:51:57 +0700

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,1 +1,19 @@
+def fib(k: int) -> int:
+    '''
+    Recursive Fibonacci sequence.
+    '''
+    if k == 0:
+        return 0
+    elif k == 1:
+        return 1
+    else:
+        return fib(k - 1) + fib(k - 2)
+
+def main():
+    k = int(input())
+    result = fib(k)
+    print(result)
+
+if __name__ == "__main__":
+    main()
 
