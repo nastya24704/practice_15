d242d4f9b4594a31fc118599d309d2a531116587
nastya24704 <nastazeravina@gmail.com>
2025-11-25 00:55:16 +0700

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,5 +1,5 @@
 def count(n: int) -> int:
-    """
+    '''
     Recursive function to count the number of digits in a natural number.
 
     Args:
@@ -7,19 +7,19 @@ def count(n: int) -> int:
 
     Returns:
         Number of digits in the given number
-    """
+    '''
     if n <= 0:
         print('Input must be a natural number (n > 0)')
         return None
     if n < 10:
         return 1
-    return 1+count(n//10)
+    return 1 + count(n//10)
 
 
 def main():
-    """
+    '''
         Main function to demonstrate the digit counting.
-    """
+    '''
     n = int(input('Enter a natural number: '))
 
     result = count(n)
